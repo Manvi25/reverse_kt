98c968bbc803540942f7ed6eb51ef706c7822488
Manvi25 <manvi.khandelwal@diggibyte.com>
2024-05-17 22:21:04 +0530

    python practice

---
+++ src/python/lambda_function.py
@@ -0,0 +1,5 @@
+cube = lambda x: x ** 3
+number = 3
+cubed_number = cube(number)
+print(cubed_number)
+
---
+++ src/python/string_functions.py
@@ -0,0 +1,27 @@
+
+str1 = "Hello World"
+swapped = str1.swapcase()
+print(swapped)
+
+
+starts_with_hello = str1.startswith("Hello")
+print(starts_with_hello)
+
+
+split_str = str1.split(" ")
+print(split_str)
+
+
+whitespace_str = "   Hello World   "
+stripped_str = whitespace_str.strip()
+print(stripped_str)
+
+lower_str = str1.lower()
+print(lower_str)
+
+upper_str = str1.upper()
+print(upper_str)
+
+
+replaced_str = str1.replace("World", "Python")
+print(replaced_str)
